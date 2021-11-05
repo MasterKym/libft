63a6eba3a4df7344c403036f9715c88c5b1cbf27
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 10:52:20 +0100

    few prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:52:01 by mkhalid           #+#    #+#             */
-/*   Updated: 2021/11/03 12:54:29 by mkhalid          ###   ########.fr       */
+/*   Updated: 2021/11/05 10:30:47 by mkhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 # include <stdlib.h>
 
 int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+
 
 #endif
