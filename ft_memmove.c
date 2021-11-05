d141dad10ae6faa1792086c850456182ebd4d5dd
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 09:55:44 +0100

    memmove

---
+++ ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 09:36:51 by mkhalid           #+#    #+#             */
+/*   Updated: 2021/11/05 09:42:59 by mkhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*s;
+	char	*d;
+	size_t	i;
+
+	s = (char *)src;
+	d = (char *)dst;
+
+	if(s + len >= d)
+	{
+		i = len;
+		while(i--)
+			d[i] = s[i];
+	}
+	else
+	{
+		i = 0;
+		while(i < len)
+			d[i] = s[i];
+	}
+	return (dst);
+}
