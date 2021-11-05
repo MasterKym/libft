644583a3bab260e8f0c604650eecfa2f15fad36d
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 11:28:01 +0100

    prototype addition

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:52:01 by mkhalid           #+#    #+#             */
-/*   Updated: 2021/11/05 10:30:47 by mkhalid          ###   ########.fr       */
+/*   Updated: 2021/11/05 10:53:11 by mkhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
 
 
 #endif
