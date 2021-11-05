3b7166c9332cf26adad87c924e57fa71432b9ae4
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 10:27:40 +0100

    seems to work

---
+++ ft_strlcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 09:56:19 by mkhalid           #+#    #+#             */
+/*   Updated: 2021/11/05 10:06:32 by mkhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while(src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
