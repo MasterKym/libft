4683735ab43c50260d0eb6df84629418694f0f0b
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 10:51:23 +0100

    included unistd.h, not sure I should though

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,15 @@
 /*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:19:03 by mkhalid           #+#    #+#             */
-/*   Updated: 2021/11/03 15:23:09 by mkhalid          ###   ########.fr       */
+/*   Updated: 2021/11/05 10:41:54 by mkhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
  size_t isn't defined here until <unistd.h> is included
 */
+#include <unistd.h>
+
 size_t	ft_strlen(const char *s)
 {
 	int	i;
