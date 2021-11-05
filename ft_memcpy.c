1ef1aeec0574a665c54b3a10a3e777ee4f37d4ce
Mohamed yassir Khalid <mkhalid@e1r11p5.1337.ma>
2021-11-05 10:27:28 +0100

    seems to work, need to remove main

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,36 +6,37 @@
 /*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 18:35:34 by mkhalid           #+#    #+#             */
-/*   Updated: 2021/11/04 15:57:28 by mkhalid          ###   ########.fr       */
+/*   Updated: 2021/11/05 10:26:23 by mkhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    int i;
-    const void *newsrc;
+	const int	*newsrc;
 
-    newsrc = src;
-    i = 0;
-    while (i < (int)n)
-    {
-        *(char *)dst = *(char *)newsrc;
-        dst++;
-        newsrc++;
-        i++;
-    }
+	newsrc = (const int *)src;
+	while(n--)
+	{
+		*(char *)dst = *newsrc;
+		newsrc++;
+		dst++;
+	}
     return (dst);
 }
 
 int main()
 {
-    char *src = "memcpyy can be very useful....!";
+    char src[] = "memcpyy can be very useful....!";
+    char src1[] = "memcpyy can be very useful....!";
 
     ft_memcpy(src+20, src+15, 11);
-    printf("%s", src);
+    memcpy(src1+20, src1+15, 11);
+    printf("%s\n", src);
+    printf("%s", src1);
 
     return 0;
 }
