bd7bd06186c9f77f73fa38d3de1e3a27dcf2c03f
Mohamed yassir Khalid <mkhalid@e1r10p13.1337.ma>
2021-11-10 08:19:55 +0100

    lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 19:37:58 by mkhalid           #+#    #+#             */
+/*   Updated: 2021/11/09 20:03:26 by mkhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = (t_list *)malloc(sizeof(t_list));
+	if(!ret)
+		return (0);
+	ret->content = content;
+	ret->next = 0;
+	return (ret);
+}
