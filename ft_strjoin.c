6330f7b4d958150945b3f5fb31c4b0b3c7facec5
Mohamed yassir Khalid <mkhalid@e1r6p15.1337.ma>
2021-11-13 13:22:42 +0100

    fix MKO

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mkhalid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 17:44:13 by mkhalid           #+#    #+#             */
-/*   Updated: 2021/11/12 20:05:15 by mkhalid          ###   ########.fr       */
+/*   Updated: 2021/11/13 13:21:21 by mkhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		s2l;
 	char	*join;
 
-	if (!s1 || !s2)
+	if (!s1)
 		return (0);
 	s1l = ft_strlen(s1);
 	s2l = ft_strlen(s2);
-	join = malloc((sizeof(char) + 1) * (s1l + s2l));
+ 	join = malloc(sizeof(char) * (s1l + s2l + 1));
 	if (!join)
 		return (0);
-	ft_bzero((void *)join, s1l + s2l);
-	ft_strlcat(join, s1, s1l + 1);
-	ft_strlcat(join, s2, s1l + s2l + 1);
+	ft_strlcpy(join, s1, s1l + 1);
+	ft_strlcat(join + s1l, s2, s2l + 1);
 	return (join);
 }
